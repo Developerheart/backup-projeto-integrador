58167f871f9f1e84cae87465af0b6244ec63428c
Ana Paula <anapaulavieira.ac@gmail.com>
2021-10-05 10:06:55 -0500

    Teste frontend

--- app/views.py
+++ app/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
-def index():
-    context = {}
-    template = loader.get_template('app/cadastrar-usuario.html')
-    return HttpResponse(template.render(context, request))
+def index(request):
+    return render(request, 'app/index.html')
