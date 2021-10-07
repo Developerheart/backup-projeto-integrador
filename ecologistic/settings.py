b5d1369b1e65af36c16ce75f79e3d1d4d7e99ceb
Ana Paula <anapaulavieira.ac@gmail.com>
2021-10-07 10:34:30 -0500

    Correçao arquivos .html

--- ecologistic/settings.py
+++ ecologistic/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '<nome_da_aplicacao>/static')
+    os.path.join(BASE_DIR, 'app/static')
 ]
 
 # Default primary key field type
