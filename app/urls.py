b5aba8d748d64ab2b5bf5dc5548d517881702cad
Ana Paula <anapaulavieira.ac@gmail.com>
2021-10-25 20:29:17 -0500

    Templates dos links do rodape

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,9 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('app/anuncios', AnunciosView.as_view(), name='anuncios'),
     path('app/publicacoes', PublicacoesView.as_view(), name='publicacoes'),
-     path('app/inserir-anuncio', InserirAnuncioView.as_view(), name='inserir-anuncio'),
+    path('app/inserir-anuncio', InserirAnuncioView.as_view(), name='inserir-anuncio'),
+    path('app/sobre-nos', SobreNosView.as_view(), name='sobre-nos'),
+    path('app/termos-de-uso', TermosUsoView.as_view(), name='termos'),
+    path('app/central-de-ajuda', AjudaView.as_view(), name='ajuda'),
+    path('app/entre-em-contato', ContatoView.as_view(), name='contato'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,15 +6,45 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = "app/index.html"
     
-    
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        return context
+
+
+class SobreNosView(TemplateView):
+    template_name = "app/sobre-nos.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SobreNosView, self).get_context_data(**kwargs)
+        return context
+    
+
+class TermosUsoView(TemplateView):
+    template_name = "app/termos-de-uso.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(TermosUsoView, self).get_context_data(**kwargs)
+        return context
+    
+
+class AjudaView(TemplateView):
+    template_name = "app/central-de-ajuda.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(AjudaView, self).get_context_data(**kwargs)
+        return context
+    
+
+class ContatoView(TemplateView):
+    template_name = "app/entre-em-contato.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(ContatoView, self).get_context_data(**kwargs)
         return context
     
 
 class AnunciosView(TemplateView):
     template_name = "app/anuncios.html"
-    
 
     def get_context_data(self, **kwargs):
         context = super(AnunciosView, self).get_context_data(**kwargs)
@@ -23,7 +53,6 @@ class AnunciosView(TemplateView):
 
 class PublicacoesView(TemplateView):
     template_name = "app/publicacoes.html"
-    
 
     def get_context_data(self, **kwargs):
         context = super(PublicacoesView, self).get_context_data(**kwargs)
@@ -33,7 +62,6 @@ class PublicacoesView(TemplateView):
 class InserirAnuncioView(TemplateView):
     template_name = "app/inserir-anuncio.html"
 
-    
     def get_context_data(self, **kwargs):
         context = super(InserirAnuncioView, self).get_context_data(**kwargs)
         return context
